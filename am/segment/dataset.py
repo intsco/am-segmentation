fix bug in image normalization to 0-1 range

am/segment/dataset.py
@@ -64,12 +64,18 @@ class AMDataset(Dataset):
     def __len__(self):
         return len(self.mask_df)
 
+    def _read_image(self, path, one_channel):
+        image = cv2.imread(str(path))
+        if one_channel:
+            image = image[:, :, :1]  # because ch0==ch1==ch2
+        return image
+
     def __getitem__(self, idx):
         image_path = self.image_df.iloc[idx].path
-        image = cv2.imread(str(image_path))
+        image = self._read_image(image_path, one_channel=False)
         mask_path = self.mask_df.iloc[idx].path
         if mask_path.exists():
-            mask = cv2.imread(str(mask_path))[:, :, :1].astype(np.float32)
+            mask = self._read_image(mask_path, one_channel=True)
         else:
             image_shape = image.shape[-2:]
             mask = np.zeros((1,) + image_shape)
@@ -102,7 +108,8 @@ def load_ds(data_path, transform, groups=None, size=None):
             image_df = pd.concat([image_df] * mult).head(size)
             mask_df = pd.concat([mask_df] * mult).head(size)
         else:
-            image_df = image_df.head(size)
-            mask_df = mask_df.head(size)
+            inds = np.random.choice(image_df.shape[0], size, replace=False)
+            image_df = image_df.iloc[inds]
+            mask_df = mask_df.iloc[inds]
 
     return AMDataset(image_df, mask_df, transform=transform)

am/segment/train.py
@@ -6,9 +6,9 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader
 from segmentation_models_pytorch import Unet, FPN
 
-from am.segm.dataset import AMDataset, make_image_mask_dfs, train_transform, valid_transform
-from am.segm.loss import jaccard, CombinedLoss
-from am.segm.model import UNet11
+from am.segment.dataset import AMDataset, make_image_mask_dfs, train_transform, valid_transform
+from am.segment.loss import jaccard, CombinedLoss
+from am.segment.model import UNet11
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 

am/segment/utils.py
@@ -11,10 +11,6 @@ from torch.utils.data import DataLoader
 logger = logging.getLogger('am-segm')
 
 
-def read_image(path):
-    return cv2.imread(str(path))[:,:,0]  # because ch0==ch1==ch2
-
-
 def convert_to_image(array):
     if type(array) == torch.Tensor:
         image = array.detach().cpu().numpy()
@@ -39,33 +35,9 @@ def plot_images_row(images, titles=None):
 
     n = min(len(images), 4)
     fig, axes = plt.subplots(1, n, figsize=(16, 8))
-    if type(axes) == list:
+    if isinstance(axes, np.ndarray):
         for i in range(n):
             plot(axes[i], images[i], titles[i] if titles else None)
     else:
         plot(axes, images[0], titles[0] if titles else None)
     return fig
-
-
-def plot_overlay(image, mask, figsize=(10, 10)):
-    fig, ax = plt.subplots(1, 1, figsize=figsize)
-    image, mask = np.array(image), np.array(mask)
-    if image.ndim > 2:
-        image = image[0]  # plot first channel only
-    if mask.ndim > 2:
-        mask = mask[0]  # plot first channel only
-    ax.imshow(image, cmap='gray', interpolation=None)  # plot first channel only
-    ax.imshow(mask, cmap='jet', interpolation=None, alpha=0.5)
-    return fig
-
-
-def overlay_images_with_masks(path, image_ext='png'):
-    for group_path in path.iterdir():
-        logger.info(f'Overlaying: {group_path}')
-        mask = read_image(str(group_path / f'mask.{image_ext}'))
-        image = read_image(str(group_path / f'source.{image_ext}'))
-        assert image.shape == mask.shape
-
-        fig = plot_overlay(image, mask)
-        plt.savefig(group_path / f'overlay.{image_ext}', dpi=600, bbox_inches='tight')
-        plt.close()