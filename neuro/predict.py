1d5f9b5bb1684913e58e728419e6ff436afd0843
Vitaly Kovalev <1176502+intsco@users.noreply.github.com>
2020-11-03 21:23:59 +0100

    feat: predict.py script

--- neuro/predict.py
+++ neuro/predict.py
@@ -1,26 +1,26 @@
 import argparse
+import logging
 import math
-import os
 from pathlib import Path
+from typing import List
 
+from albumentations.pytorch.functional import img_to_tensor
+from albumentations import Compose, Normalize, Resize
 import torch
-import segmentation_models_pytorch as smp
-import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
 
-from am.ecs import AMDataset
-from am.segment.dataset import create_dl, train_transform, valid_transform
-from am.segment.loss import CombinedLoss
-from am.segment.train import train_loop, convert_history_to_tuple
-from am.segment.visual import plot_loss_metric, save_history_overlay
-from am.utils import save_model, clean_dir, load_model
+from am.logger import init_logger
+from am.utils import load_model
+from am.segment.image_utils import read_image, save_image
+
+init_logger()
+logger = logging.getLogger('am-segm')
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--batch-size', type=int, default=8)
-    parser.add_argument('--num-workers', type=int, default=4)
-
     # Data, model, and output directories
     parser.add_argument('--model-dir', type=str, default='results')
     parser.add_argument('--data-dir', type=str)
@@ -29,32 +29,64 @@ def parse_args():
     return args
 
 
+class AMDataset(Dataset):
+
+    def __init__(self, image_paths: List[Path]):
+        self._image_paths = image_paths
+        self._transform = Compose([Normalize(), Resize(512, 512)])
+
+    def __len__(self):
+        return len(self._image_paths)
+
+    def __getitem__(self, idx):
+        image_path = self._image_paths[idx]
+        image = read_image(image_path, ch_n=3)
+        image = self._transform(image=image)['image']
+        mask_path = image_path.parent.parent / 'mask' / image_path.name
+        return img_to_tensor(image), mask_path
+
+    def batches(self, batch_size=8):
+        batch_n = math.ceil(len(self) / batch_size)
+        idx = 0
+        for _ in range(batch_n):
+            items = []
+            for _ in range(batch_size):
+                if idx == len(self):
+                    break
+                items.append(self[idx])
+                idx += 1
+            yield zip(*items)
+
+
+def save_predictions(predictions, output_paths):
+    logger.debug(f'Saving {len(predictions)} predictions')
+    for pred, output_path in zip(predictions, output_paths):
+        if not output_path.parent.exists():
+            output_path.parent.mkdir(parents=True)
+        image = pred * 255
+        logger.debug(f'Saving prediction: {image.shape} to {output_path}')
+        save_image(image, output_path)
+
+
 if __name__ == '__main__':
     args = parse_args()
+    logger.info(f'Predicting image masks at {args.data_dir}')
 
     model = load_model(Path(args.model_dir) / 'model.pt')
-
-    # Load data
-    # dl = create_dl(
-    #     paths=[Path(args.data_dir)],
-    #     transform=valid_transform(),
-    #     shuffle=False,
-    #     batch_size=args.batch_size,
-    #     num_workers=args.num_workers,
-    # )
-
-    image_paths = Path(args.data_dir).iterdir()
-    batch_size = args.batch_size
-    batch_n = math.ceil(len(a) / batch_size)
-
-    AMDataset()
-
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    image_paths = []
+    for group_path in Path(args.data_dir).iterdir():
+        image_paths.extend(list((group_path / 'source').iterdir()))
+    dataset = AMDataset(image_paths)
+
     with torch.no_grad():
-        for inputs, _ in dl:
+        for images, mask_paths in dataset.batches(batch_size=args.batch_size):
             print('.', end='')
-            inputs = inputs.to(device)
+
+            inputs = torch.stack(images).to(device)
             probs = torch.sigmoid(model(inputs))
             probs = probs.squeeze(dim=1).detach().cpu().numpy()
             masks = (probs > 0.5).astype(int)
 
+            save_predictions(masks, mask_paths)
